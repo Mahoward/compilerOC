8aaca6b57f6967c6c668c46b1a3e6a9afed2679d
Michael Howard <mahoward@ucsc.edu>
2014-11-17 13:09:29 -0800

    debugging

--- astree.cpp
+++ astree.cpp
@@ -72,10 +72,6 @@ static void dump_node (FILE* outfile, astree* node) {
   fprintf (outfile, "%s \"%s\" %ld.%ld.%ld",
             tok_base_name(get_yytname (node->symbol)), node->lexinfo->c_str(),
             node->filenr, node->linenr, node->offset);
-  fprintf (outfile, "%p->{%s(%d) %ld:%ld.%03ld \"%s\" [",
-        node, get_yytname (node->symbol), node->symbol,
-        node->filenr, node->linenr, node->offset,
-        node->lexinfo->c_str());
   /*bool need_space = false;
   for (size_t child = 0; child < node->children.size(); ++child) {
     if (need_space) fprintf (outfile, " ");
