b02faf836624cd7986b761aa4a11ac390ec11d79
Michael Howard <mahoward@ucsc.edu>
2014-12-01 19:06:07 -0800

    struct

--- symtable.cpp
+++ symtable.cpp
@@ -46,7 +46,7 @@ int var_type(astree* node){
 }
 
 
-void populate_fields(astree* root, symbol_table fields){
+void populate_fields(astree* root, symbol_table& fields){
   for(size_t i = 0; i < root->children.size(); i++){
     if(root->children[i]->symbol != TOK_TYPEID){
       symbol* sym = new symbol();
@@ -83,7 +83,7 @@ void insert_struct(astree* root){
   symbol* struct_sym = new symbol();
   struct_sym->attributes.set(ATTR_struct);
   struct_sym->fields = new symbol_table();
-  populate_fields(root, &struct_sym->fields);
+  populate_fields(root, struct_sym->fields);
   string* key = populate_struct_sym(root, struct_sym);
   if(key == NULL){
     printf("KEY ERROR\n");
