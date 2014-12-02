373085763b05de3b5fbdf7d7c32a4d36099f7e2b
Michael Howard <mahoward@ucsc.edu>
2014-12-01 19:19:57 -0800

    decided to put this assignment off

--- symtable.cpp
+++ symtable.cpp
@@ -88,7 +88,7 @@ void insert_struct(astree* root){
   if(key == NULL){
     printf("KEY ERROR\n");
   }else{
-    /*printf("Key:%s\n", key->c_str());
+    printf("Key:%s\n", key->c_str());
     printf("struct_sym: %ld, %ld, %ld, %ld\n",
             struct_sym->filenr,struct_sym->linenr,
             struct_sym->offset,struct_sym->blocknr);
